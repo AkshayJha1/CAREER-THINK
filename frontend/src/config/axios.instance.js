f817f61d5651e7de5af81f115d3709e656c4eebb
Akshay Kumar Jha <akshaykumarjha26@gmail.com>
2025-06-20 17:58:24 +0530

    Update axios.instance.js

--- frontend/src/config/axios.instance.js
+++ frontend/src/config/axios.instance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000/api'
-    : 'https://careerthink-backend.onrender.com/api';
+    : 'https://career-think-backend.onrender.com/api';
 
 export const axiosInstance = axios.create({
   baseURL,
